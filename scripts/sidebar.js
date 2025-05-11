02b3793b3f89a63ab545144d6996dd3b83c46694
Lilnumb <lilnumbps@gmail.com>
2025-05-12 00:33:59 +0330

    added js for date in sidebar.js

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const sidebar = document.querySelector(".sidebar");
   const closeBtn = document.getElementById("close-sidebar");
   const overlay = document.querySelector(".sidebar__overlay");
+  const todayDate = document.getElementById("today-date");
 
+  // sidebar overlay
   function openSidebar() {
     sidebar.classList.add("active");
     overlay.classList.add("active");
@@ -19,4 +21,32 @@ document.addEventListener("DOMContentLoaded", () => {
   hamburger.addEventListener("click", openSidebar);
   closeBtn.addEventListener("click", closeSidebar);
   overlay.addEventListener("click", closeSidebar);
+
+  // date
+  if (!todayDate) return;
+
+  const today = new Date();
+
+  const formatterWeekday = new Intl.DateTimeFormat("fa-IR-u-ca-persian", {
+    weekday: "long"
+  });
+  const formatterDay = new Intl.DateTimeFormat("fa-IR-u-ca-persian", {
+    day: "numeric"
+  });
+  const formatterMonth = new Intl.DateTimeFormat("fa-IR-u-ca-persian", {
+    month: "long"
+  });
+  const formatterYear = new Intl.DateTimeFormat("fa-IR-u-ca-persian", {
+    year: "numeric"
+  });
+
+  const weekday = formatterWeekday.format(today);
+  const day = formatterDay.format(today);
+  const month = formatterMonth.format(today);
+  const year = formatterYear.format(today);
+
+  const finalDate = `${weekday}، ${day} ${month} ${year}`;
+
+  todayDate.textContent = finalDate;
+  todayDate.style.direction = "rtl";
 });
