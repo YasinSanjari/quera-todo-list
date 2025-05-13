3dbfd83e1789e1d96dab0c06be7a5c85d672a0d9
Yasin <yasinsanjari7@gmail.com>
2025-05-13 18:17:49 +0330

    feat: :art: changed addTask.js and main.js to main.js and darkMode.js

---
+++ scripts/darkMode.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const lightTheme = document.getElementById("light-theme");
+  const darkTheme = document.getElementById("dark-theme");
+
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  lightTheme.addEventListener("click", () => {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  });
+
+  darkTheme.addEventListener("click", () => {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  });
+});
